2ea6c58a074505b7b1e7c285b55f9781778c3e3e
emran40989 <emran40989@gmail.com>
2026-03-22 19:38:59 +0600

    homepage created

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+document.getElementById("login-btn").addEventListener("click", function () {
+    const number = document.getElementById("number-input").value;
+    const pin = document.getElementById("pin-input").value;
+    console.log(number, pin);
+
+
+    if (number == "01739642983" && pin == "1234") {
+        alert("login successfully")
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("login failed")
+        return;
+    }
+    
+})
